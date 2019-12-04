bce600b96867beda679f3a275a9c0dedab84c137
ShaharBlank <shahabl@ac.sce.ac.il>
2019-12-04 23:17:41 +0200

    New supporter activity

---
+++ app/src/main/java/com/example/hands_showprototype/SupporterActivity.java
@@ -0,0 +1,15 @@
+package com.example.hands_showprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class SupporterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supporter);
+    }
+}
