3cd68df39461a3ee6e4ed717d1c71eda79541c76
Dan Nereusov <danne@ac.sce.ac.il>
2019-12-08 12:25:13 +0200

    add admin and supportq files

--- app/src/main/java/com/example/hands_showprototype/AdminActivity.java
+++ app/src/main/java/com/example/hands_showprototype/AdminActivity.java
@@ -2,7 +2,9 @@ package com.example.hands_showprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
     }
+
+    public void GoToSupportQ(View view){
+        Intent SupportQ = new Intent(this, SQActivity.class);
+        startActivity(SupportQ);
+    }
+
+
+
 }
+
+
+
---
+++ app/src/main/java/com/example/hands_showprototype/SQActivity.java
@@ -0,0 +1,18 @@
+package com.example.hands_showprototype;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hands_showprototype.R;
+
+public class SQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supportq);
+    }
+
+
+}
