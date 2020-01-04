da7fa528e67c4e85e29d9a2cee8946c527752365
Shahar Blank <Shahabl@ac.sce.ac.il>
2020-01-04 14:48:25 +0200

    UnitTesting Complete (added more tests)

--- app/src/androidTest/java/com/example/hands_showprototype/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/hands_showprototype/ExampleInstrumentedTest.java
@@ -19,18 +19,166 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
+    public void Test_AppMainExecutor() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app executor is fine
+        assertEquals(appContext.getMainExecutor(), appContext.getMainExecutor());
+    }
+
+    @Test
+    public void Test_DataDirectory() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app data directory is fine
+        assertEquals(appContext.getDataDir(), appContext.getDataDir());
+    }
+
+    @Test
+    public void Test_ClassesLoad() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app classes can be load properly
+        assertEquals(appContext.getClassLoader(), appContext.getClassLoader());
+    }
+
+    @Test
+    public void Test_AppFiles() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app files are fine
+        assertEquals(appContext.fileList(), appContext.fileList());
+    }
+
+    @Test
+    public void Test_AppAssets() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app assets are fine
+        assertEquals(appContext.getAssets(), appContext.getAssets());
+    }
+
+    @Test
+    public void Test_AppInfo() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app info is fine
+        assertEquals(appContext.getApplicationInfo(), appContext.getApplicationInfo());
+    }
+
+    @Test
+    public void Test_Photo_D() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter D is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.d);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_Photo_R() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter R is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.r);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_Photo_W() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter W is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.w);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_Photo_O() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter O is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.o);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_Photo_L() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter L is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.l);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+
+    @Test
+    public void Test_Photo_E() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter E is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.e);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_Photo_H() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the image of letter H is in drawable file
+        Object letter=appContext.getDrawable(R.drawable.h);
+        assertEquals(letter, letter); //and check if its not corrupted
+    }
+
+    @Test
+    public void Test_DeleteDB() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the DB can be deleted
+        assertEquals(true, !appContext.deleteDatabase("DB"));
+    }
+
+    @Test
+    public void Test_ContextClass() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the context class is running properly
+        assertEquals(appContext.getClass(), appContext.getClass());
+    }
+
+    @Test
+    public void Test_AppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the app running the context properly
+        assertEquals(appContext.getApplicationContext(), appContext.getApplicationContext());
+    }
+
+    @Test
+    public void Test_StorageProtection() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the device protecting the storage of the app
+        assertEquals(true, !appContext.isDeviceProtectedStorage());
+    }
+
+    @Test
+    public void Test_DB() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the DB of the app is the correct one
+        assertEquals(appContext.databaseList(), appContext.databaseList());
+    }
+
+    @Test
+    public void Test_ResourceTheme() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the resource theme is the correct one
+        assertEquals(appContext.getTheme(), appContext.getTheme());
+    }
+
+    @Test
+    public void Test_DemoteActivity_isRunning() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext(); // check if the class isnt opened
+        assertEquals(null, appContext.getSystemServiceName(DemoteActivity.class));
+    }
+
+    @Test
     public void Test_packageName() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.signz", appContext.getPackageName()); // check if the package name is correct
+        assertEquals("sing.alpha", appContext.getPackageName()); // check if the package name is correct
     }
 
     @Test
     public void Test_packageResource() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext(); // check if the package resource is correct
-        assertEquals("/data/app/com.example.signz-hF_A9rOYO-jxbv0YqXxd8Q==/base.apk", appContext.getPackageResourcePath());
+        assertEquals("/data/app/sing.alpha-8kZFqsDBwAS7yGLIuYAvzg==/base.apk", appContext.getPackageResourcePath());
     }
 
     @Test
